04495db1b0528f71a90fb0f3e6a539bf0a5690e2
Manolo Stocchi <manolo.stocchi@MbPro.local>
2018-03-03 17:10:28 +0100

    added eslint config

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-'use strict';
+'use strict'
 
-var Hapi = require('hapi');
+var Hapi = require('hapi')
 
 const server = new Hapi.Server({
-    host: '0.0.0.0',
-    port: 3000
-});
+  host: '0.0.0.0',
+  port: 3000
+})
 
+server.route({
+  method: 'GET',
+  path: '/test',
+  handler: function (request, h) {
+    return 'reply from test'
+  }
+})
 
 const main = async () => {
-    try {
-        await server.start();
-        console.log('Server is running at ' + server.info.uri);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await server.start()
+    console.log('Server is running at ' + server.info.uri)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-main();
+main()
